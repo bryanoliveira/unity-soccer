8c75909103fa5897859746ae6ba8ea463babe01b
Bryan Lincoln <bryanufg@gmail.com>
2021-11-13 10:51:19 -0300

    feat: fix agent start position for watch

--- Assets/Scripts/Soccer/SoccerEnvController.cs
+++ Assets/Scripts/Soccer/SoccerEnvController.cs
@@ -131,10 +131,13 @@ public class SoccerEnvController : MonoBehaviour
 
     public void ResetBall()
     {
-        var randomPosX = Random.Range(-2.5f, 2.5f);
-        var randomPosZ = Random.Range(-2.5f, 2.5f);
-
-        ball.transform.position = m_BallStartingPos + new Vector3(randomPosX, 0f, randomPosZ);
+        ball.transform.position = m_BallStartingPos;
+        if (!isVisualizer)
+            ball.transform.position += new Vector3(
+                Random.Range(-2.5f, 2.5f),
+                0f,
+                Random.Range(-2.5f, 2.5f)
+            );
         ballRb.velocity = Vector3.zero;
         ballRb.angularVelocity = Vector3.zero;
 
@@ -220,11 +223,14 @@ public class SoccerEnvController : MonoBehaviour
         //Reset Agents
         foreach (var item in AgentsList)
         {
-            var randomPosX = Random.Range(-5f, 5f);
-            var newStartPos = item.Agent.initialPos + new Vector3(randomPosX, 0f, 0f);
-            var rot = item.Agent.rotSign * Random.Range(80.0f, 100.0f);
-            var newRot = Quaternion.Euler(0, rot, 0);
-            item.Agent.transform.SetPositionAndRotation(newStartPos, newRot);
+            var newStartPos = item.Agent.initialPos;
+            var rot = item.Agent.rotSign * 90.0f;
+            if (!isVisualizer)
+            {
+                newStartPos += new Vector3(Random.Range(-5f, 5f), 0f, 0f);
+                rot *= Random.Range(0.85f, 1.15f);
+            }
+            item.Agent.transform.SetPositionAndRotation(newStartPos, Quaternion.Euler(0, rot, 0));
 
             item.Rb.velocity = Vector3.zero;
             item.Rb.angularVelocity = Vector3.zero;
